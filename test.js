86d17e34964117e9fb2dd8ee7661f26fe6f5ce7c
zhaojingjing <zhaojingjing@xiaoyouzi.com>
2018-11-21 10:14:33 +0800

    优化无参数情况

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+// #!/usr/bin/env node 表示指定node来执行该脚本。
+// 所以可以直接使用 ./test.js来执行脚本。 
+// ./表示从path中查找
+
 const program = require('commander');
 const fs = require('fs');
 const path = require('path');
@@ -10,14 +14,13 @@ program
   .option('-f, --file [type]', 'file name')
   .parse(process.argv);
 
-
-if (!program.dir && !program.file) {
+if (process.argv.length === 2) {  // 执行脚本时未输入任何参数，则显示 -h 的内容
   program.help();
-  return;
+  process.exit(1);
 }
 
 const dir = path.join(program.dir || '', program.file || '');
-fs.readFile(dir, 'utf8', (error, data) => {
+fs.readFile(dir, 'utf8', (error, data) => {  // 读取指定目录下的文件内容
   if (error) throw error;
   console.log(data);
 });
